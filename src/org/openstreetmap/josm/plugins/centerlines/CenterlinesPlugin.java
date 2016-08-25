67170b67fa26d7b85a19475b32fcadb86bd323f8
Marcos Dione <mdione@grulic.org.ar>
2016-08-25 15:34:56 +0200

    [+] first steps into developing a JOSM plugin.

---
+++ src/org/openstreetmap/josm/plugins/centerlines/CenterlinesPlugin.java
@@ -0,0 +1,14 @@
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class CenterlinesPlugin extends Plugin {
+    /**
+     * Will be invoked by JOSM to bootstrap the plugin
+     *
+     * @param info  information about the plugin and its local installation
+     */
+    public CenterlinesPlugin(PluginInformation info) {
+        super(info);
+        // init your plugin
+    }
+}
